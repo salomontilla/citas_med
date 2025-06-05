21384cf11927200a7cad85504a04415fb6da6944
salo <salomontilla@outlook.com>
2025-06-05 14:58:48 -0500

    feat: add password input component and update registration form with new fields

--- citas-med/src/app/ui/registerForm.tsx
+++ citas-med/src/app/ui/registerForm.tsx
@@ -1,124 +1,121 @@
 'use client';
-import { Button, DatePicker } from "@heroui/react";
-import React, { useState } from "react";
+import { Button, DatePicker, Input } from "@heroui/react";
+import React, {useState } from "react";
+import {CalendarDate} from "@internationalized/date";
+import PasswordInput from "./passwordInput";
 
 export default function RegisterForm() {
-  const [form, setForm] = useState({
-    nombre: "",
-    correo: "",
-    cedula: "",
-    telefono: "",
-    fechaNacimiento: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
+  const [name, setName] = useState("");
+  const [id, setId] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [telefono, setTelefono] = useState("");
+  const [contra, setContra] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [fecha, setFecha] = useState<CalendarDate | null>(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(name, id ,correo, telefono, contra, confirm, fecha?.toString());
 
-    if (form.password !== form.confirmPassword) {
-      alert("Las contraseñas no coinciden");
-      return;
-    }
+    
 
     // Aquí iría la lógica para enviar datos al backend
-    console.log("Registrando usuario:", form);
   };
 
   return (
     <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4 items-end ">
       <div>
-        <label htmlFor="nombre" className="font-semibold">Nombre</label>
-        <input
+        <Input
+          value={name}
+          onValueChange={setName}
+          label="Nombres y apellidos"
+          labelPlacement="outside"
+          color="default"
+          size="lg"
           type="text"
-          id="nombre"
-          name="nombre"
-          value={form.nombre}
-          onChange={handleChange}
+          errorMessage="Campo requerido"
           required
-          
-          placeholder="Tu nombre completo"
-        />
+           />
       </div>
 
       <div>
-        <label htmlFor="correo" className="block font-semibold ">Correo electrónico</label>
-        <input
+        <Input
+          
+          onValueChange={setCorreo}
+
+          label="Correo electrónico"
+          labelPlacement="outside"
+          color="default"
+          size="lg"
           type="email"
-          id="correo"
-          name="correo"
-          value={form.correo}
-          onChange={handleChange}
+          errorMessage="Campo requerido o inválido"
           required
+           />
+      </div>
+
+      <div>
+        <Input
+          onValueChange={setId}
           
-          placeholder="ejemplo@correo.com"
-        />
+
+          inputMode="numeric"
+          pattern="[0-9]*"
+          label="Número de identificación"
+          labelPlacement="outside"
+          color="default"
+          size="lg"
+          type="number"
+          errorMessage="Campo requerido"
+          required
+           />
       </div>
 
       <div>
-        <label htmlFor="cedula" className="block font-semibold">Cédula</label>
-        <input
-          type="text"
-          id="cedula"
-          name="cedula"
-          value={form.cedula}
-          onChange={handleChange}
-          required
-          placeholder="Número de cédula"
-        />
-      </div>
-
-      <div>
-        <label htmlFor="telefono" className="block font-semibold ">Número telefónico</label>
-        <input
-          type="tel"
-          id="telefono"
-          name="telefono"
-          value={form.telefono}
-          onChange={handleChange}
-          required
+        <Input
+          onValueChange={setTelefono}
           
-          placeholder="Ej: 3001234567"
-        />
+          inputMode="numeric"
+          pattern="[0-9]*"
+          label="Número de teléfono"
+          labelPlacement="outside"
+          color="default"
+          size="lg"
+          type="number"
+          errorMessage="Campo requerido"
+          required
+           />
+      </div>
+      <div>
+        <PasswordInput/>
       </div>
 
       <div>
-        <label htmlFor="password" className="block font-semibold ">Fecha de nacimiento</label>
-        <DatePicker size="lg" color="primary" className="max-w-[284px] rounded-2xl"/>
-      </div>
-
-      <div>
-        <label htmlFor="password" className="block font-semibold ">Contraseña</label>
-        <input
+        <Input
+          onValueChange={setConfirm}
+          
+          label="Confirmar contraseña"
+          labelPlacement="outside"
+          color="default"
+          size="lg"
           type="password"
-          id="password"
-          name="password"
-          value={form.password}
-          onChange={handleChange}
+          errorMessage="Campo requerido"
           required
-          
-          placeholder="********"
-        />
+           />
       </div>
-
       <div>
-        <label htmlFor="confirmPassword" className="block font-semibold ">Confirmar contraseña</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          value={form.confirmPassword}
-          onChange={handleChange}
-          required
-          placeholder="********"
-        />
+        <DatePicker 
+        value={fecha}
+        onChange={(value) => setFecha(value)}
+        size="lg"  
+        label="Fecha de nacimiento"
+        labelPlacement="inside"
+        isRequired
+        className="max-w-[284px] rounded-2xl"/>
       </div>
 
        <Button
+       type="submit"
+       
       className="p-6"
       radius="lg"
       color="primary"
