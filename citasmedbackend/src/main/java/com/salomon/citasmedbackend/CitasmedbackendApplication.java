2eacf7ae50226b54ec37357671cb138be7d1d7f3
salo <salomontilla@outlook.com>
2025-06-07 18:45:01 -0500

    feat: update .gitignore to include backend and IDE-specific files

---
+++ citasmedbackend/src/main/java/com/salomon/citasmedbackend/CitasmedbackendApplication.java
@@ -0,0 +1,13 @@
+package com.salomon.citasmedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasmedbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasmedbackendApplication.class, args);
+	}
+
+}
---
+++ citasmedbackend/src/test/java/com/salomon/citasmedbackend/CitasmedbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salomon.citasmedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasmedbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
