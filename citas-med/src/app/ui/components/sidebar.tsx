ac1d1e259af9b7d3b028ca9a7015e863ec7ebe61
salo <salomontilla@outlook.com>
2025-07-02 11:30:55 -0500

    feat: implement sidebar component with navigation options

---
+++ citas-med/src/app/ui/components/sidebar.tsx
@@ -0,0 +1,197 @@
+'use client'
+import React, { useState } from "react";
+import { CalendarDays, ClipboardList, UserCog, LogOut, Menu, X } from 'lucide-react';
+import { motion } from "framer-motion";
+
+
+
+export const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  const [selected, setSelected] = useState("Agendar Cita");
+
+  return (
+    <motion.nav
+      layout
+      className="sticky top-0 h-screen shrink-0 border-r border-slate-300 bg-white p-2"
+      style={{
+        width: open ? "225px" : "fit-content",
+      }}
+    >
+      <TitleSection open={open} />
+
+      <div className="space-y-1">
+        <Option
+          Icon={CalendarDays}
+          title="Agendar Cita"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+        
+        <Option
+          Icon={ClipboardList}
+          title="Mis citas"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+        <Option
+          Icon={UserCog}
+          title="Mis datos"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+        <Option
+          Icon={LogOut}
+          title="Cerrar sesión"
+          selected={selected}
+          setSelected={setSelected}
+          open={open}
+        />
+      </div>
+
+      <ToggleClose open={open} setOpen={setOpen} />
+    </motion.nav>
+  );
+};
+
+type OptionProps = {
+  Icon: React.ElementType;
+  title: string;
+  selected: string;
+  setSelected: (title: string) => void;
+  open: boolean;
+};
+
+const Option = ({ Icon, title, selected, setSelected, open }: OptionProps) => {
+  return (
+    <motion.button
+      layout
+      onClick={() => setSelected(title)}
+      className={`relative flex h-10 w-full items-center rounded-md transition-colors ${selected === title ? "bg-indigo-100 text-indigo-800" : "text-slate-500 hover:bg-slate-100"}`}
+    >
+      <motion.div
+        layout
+        className="grid h-full w-10 place-content-center text-lg"
+      >
+        <Icon />
+      </motion.div>
+      {open && (
+        <motion.span
+          layout
+          initial={{ opacity: 0, y: 12 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.125 }}
+          className="text-xs font-medium"
+        >
+          {title}
+        </motion.span>
+      )}
+
+      { open && (
+        <motion.span
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{
+            opacity: 1,
+            scale: 1,
+          }}
+          style={{ y: "-50%" }}
+          transition={{ delay: 0.5 }}
+          className="absolute right-2 top-1/2 size-4 rounded bg-indigo-500 text-xs text-white"
+        >
+         
+        </motion.span>
+      )}
+    </motion.button>
+  );
+};
+
+type TitleSectionProps = {
+    open: boolean;
+    };  
+const TitleSection = ({open}:TitleSectionProps) => {
+  return (
+    <div className="mb-3 border-b border-slate-300 pb-3">
+      <div className="flex cursor-pointer items-center justify-between rounded-md transition-colors hover:bg-slate-100">
+        <div className="flex items-center gap-2">
+          <Logo />
+          {open && (
+            <motion.div
+              layout
+              initial={{ opacity: 0, y: 12 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.125 }}
+            >
+              <span className="block text-xs font-semibold">TomIsLoading</span>
+              <span className="block text-xs text-slate-500">Pro Plan</span>
+            </motion.div>
+          )}
+        </div>
+        {open && <X className="mr-2" />}
+      </div>
+    </div>
+  );
+};
+
+const Logo = () => {
+  return (
+    <motion.div
+      layout
+      className="grid size-10 shrink-0 place-content-center rounded-md bg-indigo-600"
+    >
+      <svg
+        width="24"
+        height="auto"
+        viewBox="0 0 50 39"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="fill-slate-50"
+      >
+        <path
+          d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
+          stopColor="#000000"
+        ></path>
+        <path
+          d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
+          stopColor="#000000"
+        ></path>
+      </svg>
+    </motion.div>
+  );
+};
+type ToggleCloseProps = {
+  open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const ToggleClose = ({ open, setOpen }:ToggleCloseProps) => {
+  return (
+    <motion.button
+      layout
+      onClick={() => setOpen((pv) => !pv)}
+      className="absolute bottom-0 left-0 right-0 border-t border-slate-300 transition-colors hover:bg-slate-100"
+    >
+      <div className="flex items-center p-2">
+        <motion.div
+          layout
+          className="grid size-10 place-content-center text-lg"
+        >
+          <Menu
+            className={`transition-transform ${open && "rotate-180"}`}
+          />
+        </motion.div>
+        {open && (
+          <motion.span
+            layout
+            initial={{ opacity: 0, y: 12 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.125 }}
+            className="text-xs font-medium"
+          >
+            Hide
+          </motion.span>
+        )}
+      </div>
+    </motion.button>
+  );
+};
